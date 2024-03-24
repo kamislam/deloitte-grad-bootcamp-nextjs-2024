7c68d31454bc193c9c592e0e9f853d13e4c87779
kamrul91111 <kamrul.islam91111@gmail.com>
2024-03-24 18:37:33 +1100

    added api route

---
+++ app/api/notes/route.ts
@@ -0,0 +1,3 @@
+export async function GET(Request: Request) {
+  return Response.json({ message: "This is a note" });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,21 @@ async function fetchProducts() {
   return response.json();
 }
 
+async function fetchNote() {
+  const response = await fetch(`${process.env.BASE_URL}/api/notes`, {
+    next: { revalidate: 5 },
+  });
+
+  return response.json();
+}
+
 export default async function Home() {
   const { products } = (await fetchProducts()) || {};
+  const { message } = (await fetchNote()) || {};
 
   return (
     <main className={styles.main}>
+      {message}
       <section>
         <ProductsSection products={products} />
       </section>
